5ca755ef9a88db3f2e401cf20623852a54929601
K3vR0ld4n <107887773+K3vR0ld4n@users.noreply.github.com>
2024-05-29 19:21:50 +0000

    Kevin Roldan - error 15

--- python.py
+++ python.py
@@ -80,10 +80,12 @@ class Vacation_:
 
     def sum(self):
         # sum the cost of the vacation package here
-        if not self.myclass.validThis(self.dist) or not self.passagner.validNumber() or not self.total_TIME.is_valid_total_TIME():
+        if (not self.myclass.validThis(self.dist) or not
+            self.passagner.validNumber() or not
+                self.total_TIME.is_valid_total_TIME()):
             return -1
         
-        #sum the total cost
+        # sum the total cost
         numberTotal = self.costBas
         numberTotal += self.myclass.get_extraCost(self.dist)
         numberTotal += self.total_TIME.getFee()
